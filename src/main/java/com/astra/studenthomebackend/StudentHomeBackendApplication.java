0530d6e01c256db58d1a9f91c89449d51c184c3b
DAlembert721 <dalembert.dmu@gmail.com>
2021-09-29 18:19:38 -0500

    first commit

---
+++ src/main/java/com/astra/studenthomebackend/StudentHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.astra.studenthomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentHomeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astra/studenthomebackend/StudentHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astra.studenthomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHomeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
