f534490ad30912166fc1150105a5bf8477e1f1c2
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-09-30 22:23:28 -0500

    refactor: User service

--- src/main/java/com/astra/studenthomebackend/accounts/service/UserServiceImpl.java
+++ src/main/java/com/astra/studenthomebackend/accounts/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.astra.studenthomebackend.accounts.service;
 import com.astra.studenthomebackend.accounts.domain.model.auth.User;
 import com.astra.studenthomebackend.accounts.domain.repository.UserRepository;
 import com.astra.studenthomebackend.accounts.domain.service.UserService;
+import com.astra.studenthomebackend.accounts.exceptions.UserAlreadyExistsException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User create(User user) {
         if (userRepository.existsByEmail(user.getEmail())) {
-            // Lanzar error
+            throw new UserAlreadyExistsException();
         }
         return userRepository.save(user);
     }
